b40b247f2b52a9614bd3d4d048f064881ba0be49
bintul04 <lukulayb47@gmail.com>
2025-07-08 13:36:48 +0000

    W05-HomeWork

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -1,9 +1,20 @@
+// Program.cs
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Bintu Lukulay", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Bintu Lukulay", "European History", "The Causes of World War II");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
