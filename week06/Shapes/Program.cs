90e59a70a5ef92e6512ad3a02a08bd5e5390ee41
bintul04 <lukulayb47@gmail.com>
2025-07-14 14:02:45 +0000

    W06-Shape

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        // Create an empty list of Shape
+        List<Shape> shapes = new List<Shape>();
+
+        // Create shape instances
+        Square sq = new Square("Red", 5);
+        Rectangle re = new Rectangle("Blue", 4, 6);
+        Circle ci = new Circle("Green", 3);
+
+        // Add shapes to the list
+        shapes.Add(sq);
+        shapes.Add(re);
+        shapes.Add(ci);
+
+        // Iterate through the list and display color and area
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area:F2}.");
+        }
     }
 }
